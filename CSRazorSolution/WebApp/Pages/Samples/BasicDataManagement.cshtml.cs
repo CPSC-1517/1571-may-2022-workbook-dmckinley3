50cbc18eb823256b472f7c82a3a45c4b5757592d
dmckinley3 <dmckinley3@studentmail.nait.ca>
2022-06-21 12:03:44 -0600

    copying instructor code for basicdatamanagement.cshtml.cs

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -5,29 +5,36 @@ namespace WebApp.Pages.Samples
 {
     public class BasicDataManagementModel : PageModel
     {
-        //BindProperty is an annotation that handles two-way data transfer two-way? means output to form for display AND input from form for processing
+        // BindProperty is an annotation that handles two-way data transfer
+        // two-way? means output to form for display AND input from form for processing
         [BindProperty]
         public int Num { get; set; }
-        public string Feedback { get; set; }
         [BindProperty]
         public string MassText { get; set; }
-
+        public string Feedback { get; set; }
         public void OnGet()
         {
             if (Num == 0)
             {
-                Feedback = "Executing the OnGet method for the request";
+                Feedback = "executing the OnGet method for the Get request";
             }
             else
             {
-                Feedback = $"You entered the value {Num} displayed by OnGet()";
+                Feedback = $"You entered the value {Num} display by OnGet";
             }
-
         }
-        //TODO pull his repo and put code here
+
         public void OnPost()
         {
-            Feedback = $"You entered the value {Num} displayed by OnPost()\n" + $"Your mass text : {MassText}";
+            if (Num == 0)
+            {
+                Feedback = "executing the OnPost method for a value of zero";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} (display by OnPost)\n" +
+                $" your mass text is {MassText}";
+            }
         }
     }
 }
