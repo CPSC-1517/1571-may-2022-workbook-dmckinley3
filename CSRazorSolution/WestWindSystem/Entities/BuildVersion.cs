89cc872de7b0bbb6f6585b1aeb85caaa0deda3fa
dmckinley3 <dmckinley3@studentmail.nait.ca>
2022-06-27 18:44:54 -0600

    adding buildversion class

---
+++ CSRazorSolution/WestWindSystem/Entities/BuildVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.Entities
+{
+    public class BuildVersion
+    {
+        public int Id { get; set; }
+        public int Major { get; set; }  
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
